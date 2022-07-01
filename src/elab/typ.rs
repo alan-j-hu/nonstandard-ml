9b6f07d11b3c549b596d681d8c48c7f108228897
Alan Hu <alanh@ccs.neu.edu>
2022-07-01 03:33:23 -0400

    Occurs check

--- src/elab/typ.rs
+++ src/elab/typ.rs
@@ -1,10 +1,44 @@
 use std::cell::RefCell;
+use std::cmp::{Ord, Ordering};
 use std::rc::Rc;
 
 #[derive(Copy, Clone)]
 pub struct Var {
     level: i32,
     id: i32,
+    rank: i32,
+}
+
+impl Var {
+    fn occurs(self, ty: &Type) -> Result<(), ()> {
+        match ty.find() {
+            Repr::Solved(Expr::Arrow(dom, codom)) => {
+                self.occurs(&dom)?;
+                self.occurs(&codom)
+            }
+            Repr::Solved(Expr::Integer) => Ok(()),
+            Repr::Solved(Expr::Product(typs)) => {
+                for typ in typs {
+                    self.occurs(&typ)?;
+                }
+                Ok(())
+            }
+            Repr::Solved(Expr::String) => Ok(()),
+            Repr::Unsolved(var) => {
+                if self.id == var.id {
+                    Err(())
+                } else {
+                    if self.level < var.level {
+                        *ty.inner.borrow_mut() = State::Unsolved(Var {
+                            level: self.level,
+                            ..var
+                        });
+                    }
+                    Ok(())
+                }
+            }
+        }
+    }
 }
 
 #[derive(Clone)]
@@ -14,6 +48,7 @@ enum State {
     Unsolved(Var),
 }
 
+/// The representative element
 pub enum Repr {
     Solved(Expr<Type>),
     Unsolved(Var),
@@ -32,6 +67,7 @@ impl Builder {
         Type::unsolved(Var {
             id,
             level: self.level,
+            rank: 0,
         })
     }
 
@@ -46,13 +82,13 @@ pub struct Type {
 }
 
 impl Type {
-    pub fn unsolved(var: Var) -> Type {
+    fn unsolved(var: Var) -> Type {
         Type {
             inner: Rc::new(RefCell::new(State::Unsolved(var))),
         }
     }
 
-    pub fn solved(e: Expr<Type>) -> Type {
+    fn solved(e: Expr<Type>) -> Type {
         Type {
             inner: Rc::new(RefCell::new(State::Solved(e))),
         }
@@ -69,73 +105,97 @@ impl Type {
         }
     }
 
-    pub fn unify(self, other: Type) -> Result<(), Difference> {
+    pub fn unify(self, other: Type) -> Result<(), Diff> {
         match (self.find(), other.find()) {
             (Repr::Solved(e1), Repr::Solved(e2)) => match (e1, e2) {
                 (Expr::Arrow(ty1, ty2), Expr::Arrow(ty3, ty4)) => {
                     match unify4(ty1, ty2, ty3, ty4) {
                         None => Ok(()),
-                        Some((diff1, diff2)) => Err(Difference(DiffType::Later(Box::new(
-                            Expr::Arrow(diff1, diff2),
-                        )))),
+                        Some((diff1, diff2)) => {
+                            Err(Diff::Later(Box::new(Expr::Arrow(diff1, diff2))))
+                        }
                     }
                 }
                 (Expr::Integer, Expr::Integer) => Ok(()),
                 (Expr::Product(tys1), Expr::Product(tys2)) => {
                     if tys1.len() != tys2.len() {
-                        Err(Difference(DiffType::Diff(self, other)))
+                        Err(Diff::Diff(self, other))
                     } else {
                         match unify_many(tys1.iter(), tys2.iter()) {
                             Ok(()) => Ok(()),
-                            Err(diffs) => {
-                                Err(Difference(DiffType::Later(Box::new(Expr::Product(diffs)))))
-                            }
+                            Err(diffs) => Err(Diff::Later(Box::new(Expr::Product(diffs)))),
                         }
                     }
                 }
                 (Expr::String, Expr::String) => Ok(()),
-                (_, _) => Err(Difference(DiffType::Diff(self, other))),
+                (_, _) => Err(Diff::Diff(self, other)),
             },
-            (Repr::Solved(e), Repr::Unsolved(_)) => {
-                *other.inner.borrow_mut() = State::Solved(e);
-                Ok(())
-            }
-            (Repr::Unsolved(_), Repr::Solved(e)) => {
-                *self.inner.borrow_mut() = State::Solved(e);
-                Ok(())
-            }
-            (Repr::Unsolved(_), Repr::Unsolved(_)) => {
-                *self.inner.borrow_mut() = State::Parent(other);
-                Ok(())
+            (Repr::Solved(e), Repr::Unsolved(var)) => match var.occurs(&self) {
+                Ok(()) => {
+                    *other.inner.borrow_mut() = State::Solved(e);
+                    Ok(())
+                }
+                Err(()) => Err(Diff::OccursLeft(self, var)),
+            },
+            (Repr::Unsolved(var), Repr::Solved(e)) => match var.occurs(&other) {
+                Ok(()) => {
+                    *self.inner.borrow_mut() = State::Solved(e);
+                    Ok(())
+                }
+                Err(()) => Err(Diff::OccursRight(var, other)),
+            },
+            (Repr::Unsolved(self_var), Repr::Unsolved(other_var)) => {
+                if self_var.id == other_var.id {
+                    Ok(())
+                } else {
+                    // Var of greater rank becomes parent
+                    let (lhs, lvar, rhs, rvar) = match self_var.rank.cmp(&other_var.rank) {
+                        Ordering::Less => (self, self_var, other, other_var),
+                        Ordering::Greater => (other, other_var, self, self_var),
+                        Ordering::Equal => {
+                            *other.inner.borrow_mut() = State::Unsolved(Var {
+                                rank: other_var.rank + 1,
+                                ..other_var
+                            });
+                            (self, self_var, other, other_var)
+                        }
+                    };
+                    if lvar.level < rvar.level {
+                        *rhs.inner.borrow_mut() = State::Unsolved(Var {
+                            level: lvar.level,
+                            ..rvar
+                        })
+                    }
+                    *lhs.inner.borrow_mut() = State::Parent(rhs);
+                    Ok(())
+                }
             }
         }
     }
 }
 
-pub fn unify4(ty1: Type, ty2: Type, ty3: Type, ty4: Type) -> Option<(Difference, Difference)> {
+pub fn unify4(ty1: Type, ty2: Type, ty3: Type, ty4: Type) -> Option<(Diff, Diff)> {
     match (ty1.clone().unify(ty3), ty2.clone().unify(ty4)) {
         (Ok(()), Ok(())) => None,
-        (Ok(()), Err(diff)) => Some((Difference(DiffType::Same(ty1)), diff)),
-        (Err(diff), Ok(())) => Some((diff, Difference(DiffType::Same(ty2)))),
+        (Ok(()), Err(diff)) => Some(((Diff::Same(ty1)), diff)),
+        (Err(diff), Ok(())) => Some((diff, Diff::Same(ty2))),
         (Err(diff1), Err(diff2)) => Some((diff1, diff2)),
     }
 }
 
-pub fn unify_many<'a, I1, I2>(lhs: I1, rhs: I2) -> Result<(), Vec<Difference>>
+pub fn unify_many<'a, I1, I2>(lhs: I1, rhs: I2) -> Result<(), Vec<Diff>>
 where
     I1: Iterator<Item = &'a Type>,
     I2: Iterator<Item = &'a Type>,
 {
-    let diffs: Vec<Difference> = lhs
+    let diffs: Vec<Diff> = lhs
         .zip(rhs)
         .map(|(lhs, rhs)| match lhs.clone().unify(rhs.clone()) {
-            Ok(()) => Difference(DiffType::Same(lhs.clone())),
+            Ok(()) => Diff::Same(lhs.clone()),
             Err(diff) => diff,
         })
         .collect();
-    let all_same = diffs
-        .iter()
-        .all(|x| matches!(x, Difference(DiffType::Same(_))));
+    let all_same = diffs.iter().all(|x| matches!(x, Diff::Same(_)));
     if all_same {
         Ok(())
     } else {
@@ -154,12 +214,12 @@ pub enum Expr<T> {
 #[derive(Clone)]
 pub struct Forall(pub Type);
 
-pub struct Difference(DiffType);
-
-enum DiffType {
+pub enum Diff {
     Same(Type),
     Diff(Type, Type),
-    Later(Box<Expr<Difference>>),
+    OccursLeft(Type, Var),
+    OccursRight(Var, Type),
+    Later(Box<Expr<Diff>>),
 }
 
 #[cfg(test)]
@@ -196,5 +256,35 @@ mod tests {
         assert!(matches!(us.clone().find(), Repr::Unsolved(_)));
         assert!(a1.clone().unify(a2.clone()).is_ok());
         assert!(matches!(us.clone().find(), Repr::Solved(_)));
+    }
+
+    #[test]
+    fn occurs_left() {
+        let mut builder = Builder::default();
+        let int = builder.solved(Expr::Integer);
+        let us = builder.unsolved();
+        let a = builder.solved(Expr::Arrow(int.clone(), us.clone()));
+        assert!(matches!(us.clone().find(), Repr::Unsolved(_)));
+        assert!(a.unify(us).is_err());
+    }
+
+    #[test]
+    fn occurs_right() {
+        let mut builder = Builder::default();
+        let int = builder.solved(Expr::Integer);
+        let us = builder.unsolved();
+        let a = builder.solved(Expr::Arrow(int.clone(), us.clone()));
+        assert!(matches!(us.clone().find(), Repr::Unsolved(_)));
+        assert!(us.unify(a).is_err());
+    }
+
+    #[test]
+    fn occurs_refl() {
+        let mut builder = Builder::default();
+        let int = builder.solved(Expr::Integer);
+        let us = builder.unsolved();
+        assert!(matches!(us.clone().find(), Repr::Unsolved(_)));
+        assert!(us.clone().unify(us.clone()).is_ok());
+        assert!(matches!(us.find(), Repr::Unsolved(_)));
     }
 }
